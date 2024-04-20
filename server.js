b909d88471b37573d0834a9bb4f09a9f523e85dd
ChristinaHart888 <christinahart888@gmail.com>
2024-04-20 18:09:39 +0800

    Added addsong

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const io = require("socket.io")(8080, {
-	cors: {
-		origin: ["http://localhost:3000"],
-	},
+const { Server } = require("socket.io");
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000",
+    },
 });
 
+io.listen(8080);
+
 io.on("connection", (socket) => {
-	console.log(socket.id);
+    console.log(socket.id);
+    socket.on("join-room", (room) => {
+        socket.join(room);
+        console.log("Join room " + socket.id + " to room " + room);
+    });
+    socket.on("add-song", (data) => {
+        const roomId = data.roomId;
+        const userId = data.userId;
+        const link = data.link;
+        console.log("New song:", data.roomId, data.userId, data.link);
+        socket.to(roomId).emit("new-song", data);
+    });
 });
